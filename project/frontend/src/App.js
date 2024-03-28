1a8902bdcf8fe1b2fa2afa486fa4ffdb98fbb1d1
aln4ks <sadalyona@gmail.com>
2024-03-28 13:30:26 +0300

    add: создана главная страница веб-сайта

--- project/frontend/src/App.js
+++ project/frontend/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'; 
+import { Main } from './pages/main/main';
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Main>
+          <Routes>
+            <Route path='/' />
+            <Route path='/' />
+          </Routes>
+        </Main>
+      </Router>
     </div>
   );
 }
