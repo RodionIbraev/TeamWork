627237bc8d4ba3059ba8bf9be34f563556a1ed02
Rodion Ibraev <roriibra@gmail.com>
2024-05-02 14:39:55 +0300

    fix: Отображение запросов на бэке

--- project/teamwork/views/auxiliary.py
+++ project/teamwork/views/auxiliary.py
@@ -10,6 +10,8 @@ def auth_required(view_func):
     def wrapper(self, request, *args, **kwargs):
         token = request.headers.get("token")
         if not token:
+            token = request.COOKIES.get("jwt_token")
+        if not token:
             raise AuthenticationFailed("Не авторизован!")
         try:
             jwt.decode(token, "secret", algorithms=["HS256"])
@@ -23,6 +25,8 @@ def auth_required(view_func):
 
 def get_user(request):
     token = request.headers.get("token")
+    if not token:
+        token = request.COOKIES.get("jwt_token")
     payload = jwt.decode(token, "secret", algorithms=["HS256"])
     user = Employee.objects.get(id=payload["id"])
     return user
