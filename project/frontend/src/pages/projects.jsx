0f7383b0223f4fff96ac8ebf7505767a25c32ae1
aln4ks <sadalyona@gmail.com>
2024-05-02 22:09:39 +0300

    add: Добавлено удаление проектов.

--- project/frontend/src/pages/projects.jsx
+++ project/frontend/src/pages/projects.jsx
@@ -3,10 +3,17 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import { Helmet } from 'react-helmet';
 import '../styles/projects.css'
+import { Trash } from 'phosphor-react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 function Projects() {
     const [projects, setProjects] = useState([]);
     const [employees, setEmployees] = useState([]);
+    const [userProfile, setUserProfile] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [showOverlay, setShowOverlay] = useState(false);
+    const [selectedProject, setSelectedProject] = useState(null);
 
     useEffect(() => {
         const fetchProjects = async () => {
@@ -35,8 +42,22 @@ function Projects() {
             }
         };
 
+        const fetchUserProfile = async () => {
+          try {
+              const responseUserProfile = await axios.get('http://127.0.0.1:8000/user-profile/', {
+                  headers: {
+                      'token': sessionStorage.getItem("accessToken"),
+                  }
+              });
+              setUserProfile(responseUserProfile.data);
+          } catch (error) {
+              console.error('Error fetching user profile:', error);
+          }
+      };
+
         fetchProjects();
         fetchEmployees();
+        fetchUserProfile();
     }, []);
 
     const formatDate = (dateString) => {
@@ -48,18 +69,49 @@ function Projects() {
     };
 
     const getEmployeeName = (creatorId) => {
-        const creator = employees.find(employee => employee.id === creatorId);
-        if (creator) {
-            return `${creator.first_name} ${creator.last_name}`;
-        }
-        return 'Unknown';
+      const creator = employees.find(employee => employee.id === creatorId);
+      if (creator) {
+          return `${creator.first_name} ${creator.last_name}`;
+      }
+      return 'Unknown';
     };
+
+    const openModal = (project) => {
+      setSelectedProject(project);
+      setShowModal(true);
+      setShowOverlay(true);
+  };
+  
+  const closeModal = () => {
+      setShowModal(false);
+      setShowOverlay(false);
+  };
+  
+  const handleDeleteProject = async () => {
+      try {
+          await axios.delete(`http://127.0.0.1:8000/project/delete/${selectedProject.id}/`, {
+              headers: {
+                  'token': sessionStorage.getItem("accessToken"),
+              }
+          });
+          
+          setProjects(projects.filter(project => project.id !== selectedProject.id));
+  
+          toast.success("Вы удалили проект!");
+          console.log(`Проект "${selectedProject.name}" успешно удален!`);
+
+          closeModal();
+      } catch (error) {
+          console.error('Error deleting project:', error);
+      }
+  };
 
     return (
         <div className='projects'>
             <Helmet>
                 <title>Просмотр проектов</title>
             </Helmet>
+            {showOverlay && <div className="overlay" onClick={closeModal}></div>}
             {projects.map(project => (
               <div key={project.id} className='project-card'>
                 <Link key={project.id} to={`/project/${project.id}`} className='project-link'>
@@ -68,12 +120,39 @@ function Projects() {
                     <p><strong>{project.name}</strong></p>
                     <p>Создан: {formatDate(project.created_at)}</p>
                   </div>
+                  </Link>
                   <div className="description">
                     <p>{project.description}</p>
                   </div>
-                  </Link>
+                  {userProfile && userProfile.id === project.creator && (
+                    <Trash size={32} onClick={() => openModal(project)} className='project-delete'> </Trash>
+                  )}
+                  
               </div>
             ))}
+        {showModal && selectedProject && (
+            <div className="modal">
+                <div className="modal-content">
+                    <p>Вы действительно хотите удалить проект "{selectedProject.name}"?</p>
+                    <div className="modal-btns">
+                      <button onClick={handleDeleteProject}>Да</button>
+                      <button onClick={closeModal}>Нет</button>
+                    </div>
+                </div>
+            </div>
+        )}
+        <ToastContainer 
+          position="bottom-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="light"
+        />
         </div>
     );
 }
