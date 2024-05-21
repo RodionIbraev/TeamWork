e2cff75f783c89336ad10a43d58ca74f9e34b6f5
aln4ks <sadalyona@gmail.com>
2024-05-21 17:07:48 +0300

    add: Загрузка фото

--- project/frontend/src/pages/user-profile.jsx
+++ project/frontend/src/pages/user-profile.jsx
@@ -6,26 +6,49 @@ import { useNavigate } from 'react-router-dom';
 export const UserProfile = () => {
     const navigate = useNavigate();
     const [userData, setUserData] = useState(null);
+    const [photoData, setPhotoData] = useState(null);
+
+    const fetchData = async () => {
+        try {
+            const response = await axios.get('http://127.0.0.1:8000/user-profile/', {
+                headers: {
+                    'token': sessionStorage.getItem("accessToken"),
+                }
+            });
+
+            setUserData(response.data);
+        } catch (error) {
+        }
+    };
+
+    const handlePhotoChange = (event) => {
+        const reader = new FileReader();
+        reader.onload = function(event) {
+            const base64String = event.target.result.split("base64,")[1];
+            setPhotoData(base64String);
+        };
+        reader.readAsDataURL(event.target.files[0]);
+    };
+
+    const handleUploadPhoto = async () => {
+        try {
+            await axios.patch('http://127.0.0.1:8000/user-profile/', { photo: photoData }, {
+                headers: {
+                    'Content-Type': 'application/json',
+                    'token': sessionStorage.getItem("accessToken"),
+                }
+            });
+        } catch (error) {
+        }
+    };
 
     useEffect(() => {
         if (!sessionStorage.getItem("accessToken")) {
             navigate('/');
-          } else {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get('http://127.0.0.1:8000/user-profile/', {
-                    headers: {
-                        'token': sessionStorage.getItem("accessToken"),
-                        }
-                });
-
-                setUserData(response.data);
-            } catch (error) {
-            }
-        };
-
-        fetchData();
-    }}, []);
+        } else {
+            fetchData();
+        }
+    }, []);
 
     return (
         <div className="main">
@@ -42,6 +65,8 @@ export const UserProfile = () => {
                     <p>Date Joined: {new Date(userData.date_joined).toLocaleString()}</p>
                 </div>
             )}
+            <input type="file" accept=".png" onChange={handlePhotoChange} />
+            <button onClick={handleUploadPhoto} disabled={!photoData}>Загрузить</button>
         </div>
     );
 };
