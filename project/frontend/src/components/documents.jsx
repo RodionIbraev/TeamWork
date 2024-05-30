0434721287d90d0a6092a081799db2860e4965c8
aln4ks <sadalyona@gmail.com>
2024-05-30 10:12:26 +0300

    add: Удаление документов

--- project/frontend/src/components/documents.jsx
+++ project/frontend/src/components/documents.jsx
@@ -1,7 +1,10 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { X } from 'phosphor-react';
-import '../styles/documents.css'
+import '../styles/documents.css';
+import { toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { FaRegTrashAlt  } from "react-icons/fa";
 
 function Documents({ projectId, onClose }) {
     const [isOverlayVisible, setIsOverlayVisible] = useState(true);
@@ -15,12 +18,12 @@ function Documents({ projectId, onClose }) {
                         'token': sessionStorage.getItem("accessToken"),
                     }
                 });
-                
+
                 const documentsArray = Object.keys(response.data).map(key => ({
                     name: key,
                     url: response.data[key]
                 }));
-                
+
                 setDocuments(documentsArray);
             } catch (error) {
                 console.error(error);
@@ -30,6 +33,43 @@ function Documents({ projectId, onClose }) {
         fetchDocuments();
     }, [projectId]);
 
+    const deleteDocument = async (fileName) => {
+        try {
+            await axios.delete(`http://127.0.0.1:8000/project/${projectId}/documents/delete`, {
+                headers: {
+                    'token': sessionStorage.getItem("accessToken"),
+                },
+                data: {
+                    disk_file_path: fileName
+                }
+            });
+    
+            setDocuments(documents.filter(document => document.name !== fileName));
+            toast.success('Документ успешно удален!');
+        } catch (error) {
+        }
+    };
+
+    const uploadDocument = async (file) => {
+        const formData = new FormData();
+        formData.append('file', file);
+        formData.append('file_name', '');
+    
+        try {
+            await axios.put(`http://127.0.0.1:8000/project/9/documents/load`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data',
+                    'token': sessionStorage.getItem("accessToken"),
+                }
+            });
+    
+            toast.success('Документ успешно загружен на сервер!');
+        } catch (error) {
+            console.error(error);
+            toast.error('Ошибка при загрузке файла.');
+        }
+    };
+    
     const closeModal = () => {
         setIsOverlayVisible(false);
         onClose();
@@ -37,23 +77,24 @@ function Documents({ projectId, onClose }) {
 
     return (
         <div>
-        {isOverlayVisible && <div className="overlay" onClick={closeModal}/>}
-        <div className="task-modal">
-        <h2 style={{color: 'var(--white-color)'}}>Документы проекта</h2>
-        <X size={30} onClick={onClose} className='close-task-window' style={{right: '20px'}}/>
-        {documents.length > 0 && (
-            <ul className="documents">
-                {documents.map((document) => (
-                    <li key={document.id}>
-                            <a href={document.url} target="_blank" rel="noopener noreferrer">{document.name}</a>
-                    </li>
-                ))}
-            </ul>
-        )}
-        </div>
+            {isOverlayVisible && <div className="overlay" onClick={closeModal}/>}
+            <div className="task-modal">
+                <h2 style={{ color: 'var(--white-color)' }}>Документы проекта</h2>
+                <X size={30} onClick={onClose} className='close-task-window' style={{ right: '20px' }} />
+                {documents.length > 0 && (
+                    <ul className="documents">
+                        {documents.map((document, index) => (
+                            <li key={index}>
+                                <a href={document.url} target="_blank" rel="noopener noreferrer">{document.name}</a>
+                                <FaRegTrashAlt size={20} onClick={() => deleteDocument(document.name)} />
+                            </li>
+                        ))}
+                    </ul>
+                )}
+                <input type="file" onChange={(e) => uploadDocument(e.target.files[0])} />
+            </div>
         </div>
     );
 }
 
-
-export default Documents
+export default Documents;
